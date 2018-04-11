532e1eb08690462beb1796e7cf1768996ccc177a
Minsu Gim <minsugim@uw.edu>
2018-04-10 17:06:56 -0700

    Finished Project files

--- app/src/main/java/edu/washington/minsugim/activityspy/MainActivity.kt
+++ app/src/main/java/edu/washington/minsugim/activityspy/MainActivity.kt
@@ -2,11 +2,31 @@ package edu.washington.minsugim.activityspy
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i(TAG, "onCreate fired")
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.i(TAG, "onStart event fired")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.e(TAG, "We're going down, Captain!")
+    }
+
+    companion object {
+        const val TAG = "MainActivity"
     }
 }
